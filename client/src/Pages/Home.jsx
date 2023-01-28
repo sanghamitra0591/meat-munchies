0e96c252dd88cd107a763d0556a0b3a7ec5fbbba
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-01-28 19:11:02 +0530

    added middledwares

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Box, Image } from '@chakra-ui/react'
+import { Box, Image, Text } from '@chakra-ui/react'
 import banner from "../Assets/licious-banner.jpeg";
 
 const Home = () => {
   return (
     <Box>
       <Box>
-        <Image src={banner} alt="banner" />
+        <Box>
+          <Image src={banner} alt="banner" />
+        </Box>
+        <Box>
+          <Box w="85%" m="auto">
+            <Box>
+              <Text>Shop by categories</Text>
+              <Text>Freshest meats just for you</Text>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   )
