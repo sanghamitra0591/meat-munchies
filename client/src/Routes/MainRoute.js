9ecf82ca18fa9db194c5b9f4dc871ec7f1acab35
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-01-29 19:17:37 +0530

    Added nav bar

--- client/src/Routes/MainRoute.js
+++ client/src/Routes/MainRoute.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from '../Pages/Home';
+import Chicken from '../Pages/Chicken';
 
 const MainRoute = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/chicken" element={<Chicken />}></Route>
       </Routes>
     </div>
   )
